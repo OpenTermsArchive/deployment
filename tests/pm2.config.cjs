36604f6cff35eb8828e6a2e41efa8b665066fb8a
Nicolas Dupont <npg.dupont@gmail.com>
2024-05-29 10:52:09 +0200

    Update test pm2 config

--- tests/pm2.config.cjs
+++ tests/pm2.config.cjs
@@ -6,28 +6,34 @@ module.exports = {
       args: 'run start:schedule',
       max_restarts: 2,
       min_uptime: '1h', // Set a relatively high duration (more than the longest run) so that restarts that occur before this duration has elapsed are considered unstable.
-      restart_delay: 10800000,
+      restart_delay: 180 * 60 * 1000, 
     },
     {
       name: 'ota-api',
       script: 'npm',
       args: 'run start:api',
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 1000,
+      exponential_backoff_restart_delay: true
     },
     {
       name: 'ota-release',
       script: 'npm',
       args: 'run dataset:schedule',
-      max_restarts: 2,
-      min_uptime: '1h', // Set a relatively high duration (more than the longest run) so that restarts that occur before this duration has elapsed are considered unstable.
-      restart_delay: 15 * 60 * 1000,
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 1000,
+      exponential_backoff_restart_delay: true
     },
     {
       name: 'ota-federation-api',
       script: 'npm',
       args: 'run start:federation-api',
-      max_restarts: 2,
-      min_uptime: '1h', // Set a relatively high duration (more than the longest run) so that restarts that occur before this duration has elapsed are considered unstable.
-      restart_delay: 15 * 60 * 1000,
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 1000,
+      exponential_backoff_restart_delay: true
     }
   ],
 };
